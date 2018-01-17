f8a0f1d4910b061ba579d0492ef40040b17fe696
apmorey <30659268+apmorey@users.noreply.github.com>
2018-01-17 13:53:49 -0500

    Update register.php
    added/changed lines 6-10 to match up with SQL statement

--- register.php
+++ register.php
@@ -3,20 +3,22 @@
 require_once'connection.php';
 
 //"F" stands for "Form" because we are grabbing from a form
-$username = $_POST['FUsername'];
-$password = $_POST['FPassword'];
-$email = $_POST['FEmail']
+$Email = $_POST['email'];
+$pass = $_POST['pass'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$phone = $_POST['phone'];
 
 //$sql = "INSERT INTO profiles VALUES ( NULL, '$username', '$password', '$email')";
 
 //$query = $conn->prepare ( $sql );
 
 //This way may be safer
-$sql = "INSERT INTO profiles ( :pEmail, :pPass, :fname, :lname, :pPhone, :pPhoneExt ) VALUES ( Email, pass, first-name, last-name, phone, ext )";
+$sql = "INSERT INTO profiles ( :pEmail, :pPass, :fname, :lname, :pPhone ) VALUES ( Email, pass, firstname, lastname, phone )";
 
 $query = $conn->prepare( $sql );
 
-$result = $query->execute( array( ':pEmail'=>$Email, ':pPass'=>$pass, ':fname'=>$firstname, ':lname'=>$lastname, ':pPhone'=>$phone, ':pPhoneExt'=>$ext ) );
+$result = $query->execute( array( ':pEmail'=>$Email, ':pPass'=>$pass, ':fname'=>$firstname, ':lname'=>$lastname, ':pPhone'=>$phone ) );
 
 if ( $result ){
   echo "<p>Thank you. You have been registered</p>";
