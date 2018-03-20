76657089abca945f2f7032fe9bb1a85af5eb148d
nmcox <nmcox@oakland.edu>
2018-03-20 16:46:51 -0400

    Added new column to comments.sql

--- SQL/comments.sql
+++ SQL/comments.sql
@@ -5,5 +5,6 @@
 CREATE TABLE `comments` (
   `Comment_ID` int(11) NOT NULL,
   `Comment_Description` varchar(250) NOT NULL,
-  `Comment_Email` varchar(50) NOT NULL
+  `Comment_Email` varchar(50) NOT NULL,
+  `Report_ID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
